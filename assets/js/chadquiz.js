cdb2c23541d996288cd57cb1ba8b6bf9a73bdebf
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-07-30 20:12:17 -0700

    added two new quizzes

---
+++ assets/js/chadquiz.js
@@ -0,0 +1,31 @@
+function endcq1(){
+    document.getElementById("cq1").style.display = "none"
+    document.getElementById("resulta").style.display = "block"
+}
+function endcq2(){
+    document.getElementById("cq2").style.display = "none"
+    document.getElementById("resultc").style.display = "block"
+}
+function endcq3(){
+    document.getElementById("cq3").style.display = "none"
+    document.getElementById("resultd").style.display = "block"
+}
+
+
+
+function cq1a(){
+    document.getElementById("cq1").style.display = "none"
+    document.getElementById("cq2").style.display = "block"
+}
+function cq2a(){
+    document.getElementById("cq2").style.display = "none"
+    document.getElementById("cq3").style.display = "block"
+}
+function cq3a(){
+    document.getElementById("cq3").style.display = "none"
+    document.getElementById("resultb").style.display = "block"
+}
+
+
+
+
---
+++ assets/js/wokequize.js
@@ -0,0 +1,30 @@
+function endwq1(){
+    document.getElementById("wq1").style.display = "none"
+    document.getElementById("resulta").style.display = "block"
+}
+function endwq2(){
+    document.getElementById("wq2").style.display = "none"
+    document.getElementById("resulta").style.display = "block"
+}
+function endwq3(){
+    document.getElementById("wq3").style.display = "none"
+    document.getElementById("resulta").style.display = "block"
+}
+
+
+function wq1a(){
+    document.getElementById("wq1").style.display = "none"
+    document.getElementById("wq2").style.display = "block"
+}
+
+function wq2a(){
+    document.getElementById("wq2").style.display = "none"
+    document.getElementById("wq3").style.display = "block"
+}
+
+function wq3a(){
+    document.getElementById("wq3").style.display = "none"
+    document.getElementById("resultb").style.display = "block"
+}
+
+
