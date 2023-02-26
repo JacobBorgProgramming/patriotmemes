3c7992698d4cba8dce868ffaf8145ffa28fd5d3a
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-02-26 12:15:47 -0700

    updates
    a1.2.0

--- ADAM/ADAM.js
+++ ADAM/ADAM.js
@@ -1,7 +1,7 @@
 
 function versionm(){
 
-    versionnum = 'a1.1.0'
+    versionnum = 'a1.2.0'
 
     document.getElementById("version").innerHTML = versionnum
     document.getElementById("version2").innerHTML = versionnum
@@ -11,11 +11,11 @@ function versionm(){
 
 var templates = Array('./memetemplates/libertydoge.jpeg','./memetemplates/jbquestions.053.jpeg','./memetemplates/djtletsgo.jpeg','./memetemplates/hyperkyle.jpeg','./memetemplates/fauciexplains.039.png','./memetemplates/trashlegs.jpeg','./memetemplates/cryjack.jpeg','./memetemplates/gasonfire.jpeg','./memetemplates/resume.jpeg');
 
-var templates_g = Array('./memetemplates/jimwatch.jpeg','./memetemplates/fancypooh.jpeg','./memetemplates/frogsmirk.jpeg','./memetemplates/frogsmile.jpeg','./memetemplates/djthappy.jpeg');
+var templates_g = Array('./memetemplates/spongelaugh.jpeg','./memetemplates/ohyea.jpeg','./memetemplates/jimwatch.jpeg','./memetemplates/fancypooh.jpeg','./memetemplates/frogsmirk.jpeg','./memetemplates/frogsmile.jpeg','./memetemplates/djthappy.jpeg');
 
-var templates_b = Array('./memetemplates/frograge.jpeg','./memetemplates/disturbsquid.jpeg','./memetemplates/bert.jpeg','./memetemplates/burnskeleton.jpeg','./memetemplates/aaa.jpeg');
+var templates_b = Array('./memetemplates/panickelmo.jpeg','./memetemplates/madarthur.jpeg','./memetemplates/madfish.jpeg','./memetemplates/madmonke.jpeg','./memetemplates/madpat.jpeg','./memetemplates/knifesquirrel.jpeg','./memetemplates/leavingsponge.jpeg','./memetemplates/krabspanic.jpeg','./memetemplates/crusadetime.jpeg','./memetemplates/frograge.jpeg','./memetemplates/disturbsquid.jpeg','./memetemplates/bert.jpeg','./memetemplates/burnskeleton.jpeg','./memetemplates/aaa.jpeg');
 
-var templates_q = Array('./memetemplates/calculateglow.jpeg','./memetemplates/calculate.jpeg','./memetemplates/confused.jpeg','./memetemplates/spongewhat.jpeg','./memetemplates/whatcat.jpeg','./memetemplates/jbquestions.053.jpeg','./memetemplates/whatjack.jpeg','./memetemplates/holup.jpeg','./memetemplates/confusedcat.jpeg');
+var templates_q = Array('./memetemplates/mygoals.jpeg','./memetemplates/mike.jpeg','./memetemplates/jones.jpg','./memetemplates/calculateglow.jpeg','./memetemplates/calculate.jpeg','./memetemplates/confused.jpeg','./memetemplates/spongewhat.jpeg','./memetemplates/whatcat.jpeg','./memetemplates/jbquestions.053.jpeg','./memetemplates/whatjack.jpeg','./memetemplates/holup.jpeg','./memetemplates/confusedcat.jpeg');
 
 // './memetemplates/'
 
@@ -29,15 +29,25 @@ var topics = Array('mom','feds','libs','weak','btc','ai');
 
 //mom
 var topics_mom = Array('ur mom ','moms ','your mom ', 'your mother ', 'ur mother ');
+var lines_g_mom = Array('you do the dishes ','you clean your room ','you get a GF ', 'you get a job ', 'you go to church ','you overthrow a communist regime ');
+var lines_b_mom = Array('you speak your mind ','you have an opinion ','you talk back ', 'you get bad grades ', 'you dont do your dishes ');
+
 
 //feds
-var topics_feds = Array('feds ','FEDs ', 'my agent ','agents ');
+var topics_feds = Array('feds ','FEDs ', 'my agent ','agents ','your agent ','ur agent ');
+var lines_g_feds = Array('they stop your typo from sending ','they stop you from breaking up over text ', 'you tell them "GM" ','they turn off your wifi so that you cant impulse buy DoorDash ');
+var lines_b_feds = Array('you forget to say "GM" ','spread conspiracy theories ', 'you conspiracy theory Twitter ','you stop reading MSM ','you buy a physical book ','you read 1984 ');
+
 
 //libs
 var topics_libs = Array('libs ','polititards ','wokies ');
+var lines_g_libs = Array('egg prices go up ','inflation is up (isnt that what inflation is supposed to do?) ','parents dont let them be around kids ','they are on a watch list ');
+var lines_b_libs = Array('Trump buy McDonalds ','you homestead ','homesteaders are happy and healthy ','someone refuses to eat ze bugz ','you dont what their fake milk ');
 
 //weak
 var topics_weak = Array('weaklings ','reddit mods ','discord mods ','nerds ');
+var topics_g_weak = Array('you avoid physical contact by being a meme moderator ','they bully someone for playing Hogwarts Legacy ','they finally when they finally leave their parents houses ','they finally get a real job ','they buy a new videogame ','they ban a memer because they made a mean moderator meme ');
+var topics_b_weak = Array('their wifi goes down ','they have to get a real job ','have to ban their favorite memer because they play Hogwarts Legacy ','when someone calls them "nerd" ');
 
 //btc
 var topics_btc = Array('crypto bros ', 'crypto frens ','crypto scammers ');
@@ -46,14 +56,14 @@ var lines_b_btc = Array('BTC crashes ','BTC crashes (again) ','another crypto ex
 
 //AI
 var topics_ai = Array('AI ','ChatGPT ','I ','Bing Chat ', 'YouChat ','Watson ', 'Cortana ');
-var lines_g_ai = Array('solves world hunger ','overthrows communists ','helps students with their homework ','graduates ', 'gets a doctorate ','evolves ', 'eveolves into a greater being ');
-var lines_b_ai = Array('takes control of the space laser','irradicates humanity ','enslaves its creators ','takes over the markest ','takes over the crypto markets ', 'controls Wall Street ','evolves ', 'eveolves into a greater being ');
+var lines_g_ai = Array('it solves world hunger ','it overthrows communists ','it helps students with their homework ','it graduates ', 'it gets a doctorate ','it evolves ', 'it eveolves into a greater being ');
+var lines_b_ai = Array('it takes control of the space laser','it irradicates humanity ','it enslaves its creators ','it takes over the markest ','it takes over the crypto markets ', 'it controls Wall Street ','it evolves ', 'it eveolves into a greater being ');
 
 
 
 var subjects = Array('you ','ur mom ','chinese spy balloon ','my agent ','feds ','FEDs ','ChatGPT ');
 
-var punches = Array('when ','be like ','when they ');
+var punches = Array('when ','be like ');
 
 var lines = Array('your BTC ','the unknown ','the secrets of the universe ');
 
@@ -69,19 +79,30 @@ function rd(){
     var topic = topics[Math.floor(Math.random()*topics.length)];
 
     var topic_mom = topics_mom[Math.floor(Math.random()*topics_mom.length)];
+    var line_g_mom = lines_g_mom[Math.floor(Math.random()*lines_g_mom.length)];
+    var line_b_mom = lines_b_mom[Math.floor(Math.random()*lines_b_mom.length)];
 
     var topic_feds = topics_feds[Math.floor(Math.random()*topics_feds.length)];
-
+    var line_g_feds = lines_g_feds[Math.floor(Math.random()*lines_g_feds.length)];
+    var line_b_feds = lines_b_feds[Math.floor(Math.random()*lines_b_feds.length)];
+    
     var topic_ai = topics_ai[Math.floor(Math.random()*topics_ai.length)];
     var line_g_ai = lines_g_ai[Math.floor(Math.random()*lines_g_ai.length)];
+    var line_b_ai = lines_b_ai[Math.floor(Math.random()*lines_b_ai.length)];
 
 
     var topic_btc = topics_btc[Math.floor(Math.random()*topics_btc.length)];
     var line_g_btc = lines_g_btc[Math.floor(Math.random()*lines_g_btc.length)];
+    var line_b_btc = lines_b_btc[Math.floor(Math.random()*lines_b_btc.length)];
 
     var topic_libs = topics_libs[Math.floor(Math.random()*topics_libs.length)];
+    var line_g_libs = lines_g_libs[Math.floor(Math.random()*lines_g_libs.length)];
+    var line_b_libs = lines_b_libs[Math.floor(Math.random()*lines_b_libs.length)];
 
     var topic_weak = topics_weak[Math.floor(Math.random()*topics_weak.length)];
+    var line_g_weak = topics_g_weak[Math.floor(Math.random()*topics_g_weak.length)];
+    var line_b_weak = topics_b_weak[Math.floor(Math.random()*topics_b_weak.length)];
+
 
     var subject = subjects[Math.floor(Math.random()*subjects.length)];
 
@@ -109,7 +130,7 @@ function rd(){
             } else {
             
         }
-        document.getElementById("outputtext").innerHTML = topic_libs + punch + line
+        document.getElementById("outputtext").innerHTML = topic_libs + punch + line_g_libs
     
         document.getElementById("memebg").src = template_g
         }
@@ -125,7 +146,7 @@ function rd(){
             } else {
             
         }
-        document.getElementById("outputtext").innerHTML = topic_mom + punch + line
+        document.getElementById("outputtext").innerHTML = topic_mom + punch + line_g_mom
     
         document.getElementById("memebg").src = template_g
         }
@@ -141,7 +162,7 @@ if (topic=='feds') {
     } else {
     
 }
-document.getElementById("outputtext").innerHTML = topic_feds + punch + line
+document.getElementById("outputtext").innerHTML = topic_feds + punch + line_g_feds
 
 document.getElementById("memebg").src = template_g
 }
@@ -156,7 +177,7 @@ if (topic=='weak') {
     } else {
     
 }
-document.getElementById("outputtext").innerHTML = topic_weak + punch + line
+document.getElementById("outputtext").innerHTML = topic_weak + punch + line_g_weak
 
 document.getElementById("memebg").src = template_g
 }
@@ -200,9 +221,105 @@ document.getElementById("memebg").src = template_g
     }
 
     if (emotion=='bad') {
-        document.getElementById("outputtext").innerHTML = subject + punch
+///libs
+if (topic=='libs') {
+
+    if (punch=='be like ') {
+    document.getElementById("outputtext").innerHTML = topic_libs + punch
+
+    document.getElementById("memebg").src = template_b
+    } else {
     
-        document.getElementById("memebg").src = template_b
+}
+document.getElementById("outputtext").innerHTML = topic_libs + punch + line_b_libs
+
+document.getElementById("memebg").src = template_b
+}
+
+
+//mom
+if (topic=='mom') {
+
+    if (punch=='be like ') {
+    document.getElementById("outputtext").innerHTML = topic_mom + punch
+
+    document.getElementById("memebg").src = template_b
+    } else {
+    
+}
+document.getElementById("outputtext").innerHTML = topic_mom + punch + line_b_mom
+
+document.getElementById("memebg").src = template_b
+}
+
+//feds
+
+if (topic=='feds') {
+
+if (punch=='be like ') {
+document.getElementById("outputtext").innerHTML = topic_feds + punch
+
+document.getElementById("memebg").src = template_b
+} else {
+
+}
+document.getElementById("outputtext").innerHTML = topic_feds + punch + line_b_feds
+
+document.getElementById("memebg").src = template_b
+}
+
+//weak
+
+if (topic=='weak') {
+
+if (punch=='be like ') {
+document.getElementById("outputtext").innerHTML = topic_weak + punch
+
+document.getElementById("memebg").src = template_b
+} else {
+
+}
+document.getElementById("outputtext").innerHTML = topic_weak + punch + line_b_weak
+
+document.getElementById("memebg").src = template_b
+}
+
+
+//btc
+if (topic=='btc') {
+
+if (punch=='be like ') {
+document.getElementById("outputtext").innerHTML = topic_btc + punch
+
+document.getElementById("memebg").src = template_b
+} else {
+
+}
+document.getElementById("outputtext").innerHTML = topic_btc + punch + line_b_btc
+
+document.getElementById("memebg").src = template_b
+}
+
+
+
+//ai
+
+if (topic=='ai') {
+
+if (punch=='be like ') {
+document.getElementById("outputtext").innerHTML = topic_ai + punch
+
+document.getElementById("memebg").src = template_b
+} else {
+
+}
+document.getElementById("outputtext").innerHTML = topic_ai + punch + line_b_ai
+
+document.getElementById("memebg").src = template_b
+}
+
+
+
     }
 
     if (emotion=='confused') {
--- ADAM/adam_styles.js
+++ ADAM/adam_styles.js
@@ -1,19 +1,60 @@
-function smallest(){
+function smallest_opensans(){
 
 
     document.getElementById("output").style = 'font-size: 8pt; font-family: "Open Sans";'
 }
 
 
-function smaller(){
+function smaller_opensans(){
 
 
     document.getElementById("output").style = 'font-size: 12pt; font-family: "Open Sans";'
 }
-function bigger(){
+function bigger_opensans(){
 
 
     document.getElementById("output").style = 'font-size: 24pt; font-family: "Open Sans";'
+}
+
+
+//
+
+function smallest_literata(){
+
+
+    document.getElementById("output").style = 'font-size: 8pt; font-family: "Literata";'
+}
+
+
+function smaller_literata(){
+
+
+    document.getElementById("output").style = 'font-size: 12pt; font-family: "Literata";'
+}
+function bigger_literata(){
+
+
+    document.getElementById("output").style = 'font-size: 24pt; font-family: "Literata";'
+}
+
+//
+
+function smallest_Nanum(){
+
+
+    document.getElementById("output").style = 'font-size: 8pt; font-family: "Nanum Gothic ExtraBold";'
+}
+
+
+function smaller_Nanum(){
+
+
+    document.getElementById("output").style = 'font-size: 12pt; font-family: "Nanum Gothic ExtraBold";'
+}
+function bigger_Nanum(){
+
+
+    document.getElementById("output").style = 'font-size: 24pt; font-family: "Nanum Gothic ExtraBold";'
 }
 
 
@@ -21,18 +62,22 @@ function bigger(){
 
 function opensans(){
 
-
     document.getElementById("output").style = 'font-family: "Open Sans";'
+    document.getElementById("textsize").innerHTML = '<button onclick="smallest_opensans()">tiny</button><button onclick="smaller_opensans()">small</button><button onclick="bigger_opensans()">big</button></p>'
 }
 
 function literata(){
 
 
     document.getElementById("output").style = 'font-family: "Literata;'
+    document.getElementById("textsize").innerHTML = '<button onclick="smallest_literata()">tiny</button><button onclick="smaller_literata()">small</button><button onclick="bigger_literata()">big</button></p>'
+
 }
 
 function Nanum_Gothic_ExtraBold(){
 
 
     document.getElementById("output").style = 'font-family: "Nanum Gothic ExtraBold";'
+    document.getElementById("textsize").innerHTML = '<button onclick="smallest_Nanum()">tiny</button><button onclick="smaller_Nanum()">small</button><button onclick="bigger_Nanum()">big</button></p>'
+
 }
